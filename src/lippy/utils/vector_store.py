2950838d0386957ff78c00ce6356f7ee79f81103
Jerrick B <istolethisemailadress@gmail.com>
2023-07-13 03:29:18 +0000

    Mid obsidian loader fix. Issue #14

--- src/lippy/utils/vector_store.py
+++ src/lippy/utils/vector_store.py
@@ -1,34 +1,47 @@
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import UnstructuredMarkdownLoader
-from langchain.document_loaders import DirectoryLoader
-# from falcon_llm import FalconLLM
-# from langchain.chains import RetrievalQA
+# from langchain.text_splitter import MarkdownHeaderTextSplitter
+from langchain.document_loaders import UnstructuredMarkdownLoader, DirectoryLoader
+# import langchain
+# import inspect
+# print(inspect.getfile(MarkdownHeaderTextSplitter))
+from lippy.utils.obsidian_loader import ObsidianLoader
 
 # Injest documents
 # Provide retriever for use in RetrievalQA chain
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+    ("####", "Header 4"),
+    ("#####", "Header 5"),
+]
 
 class db:
     def __init__(self,chunkSize=500):
         self.pathDB = "/home/ubuntu/Tehas/lippy/data/db"
-        self.splitter = RecursiveCharacterTextSplitter(chunk_size=chunkSize, chunk_overlap=0)
         self.embeddings = HuggingFaceInstructEmbeddings()
         self.vectordb = None
 
-    def injest(self, path="/home/ubuntu/Tehas/lippy/data/vault/2 - Notes", pattern='**/*.md'):
+    def injest(self, path="/home/ubuntu/Tehas/lippy/data/vault/2 - Notes"):
         self.pathData = path
-        self.loader = DirectoryLoader(path, glob=pattern, loader_cls=UnstructuredMarkdownLoader)
+        self.loader = ObsidianLoader(path)
         docs = self.loader.load()
-        texts = self.splitter.split_documents(docs)
-        self.vectordb = Chroma.from_documents(documents=texts, embedding=self.embeddings, persist_directory=self.pathDB)
+        self.vectordb = Chroma.from_documents(documents=docs, embedding=self.embeddings, persist_directory=self.pathDB)
 
     def retriever(self):
         self.injest()
         return self.vectordb.as_retriever() if self.vectordb is not None else None
 
-# if __name__ == '__main__':
-#     llm = FalconLLM()
-#     db = db()
-#     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=db.retriever())
-#     print(qa.run("what is the procedure for measuring MLVSS"))
+if __name__ == '__main__':
+    # from falcon_llm import FalconLLM
+    # llm = FalconLLM()
+    # db = db()
+    loader = ObsidianLoader("/home/ubuntu/Tehas/lippy/data/vault/2 - Notes")
+    docs = loader.load()
+    
+    # print(inspect.getsource(ObsidianLoader))
+    
+    # texts = db.splitter.split_texts(docs)
+    print(len(docs))
+    print(docs[1])
