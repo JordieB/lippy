687876193238bfd84b3acb6d2c896caf3be8b95e
JordieB <jordie.r.belle@gmail.com>
2023-07-20 21:36:10 -0700

    refactored falcom_llm py for readability

--- src/lippy/utils/falcon_llm.py
+++ src/lippy/utils/falcon_llm.py
@@ -1,4 +1,5 @@
-from typing import Any, List, Mapping
+from typing import Any, List
+from pathlib import Path
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.llms.base import LLM
 from transformers import AutoTokenizer, pipeline
@@ -9,34 +10,56 @@ from langchain.chains import RetrievalQA
 from lippy.utils.vector_store import db
 from lippy.utils.speaker import Speaker
 
+# Define the project directory
+PROJ_DIR = Path(__file__).resolve().parents[3]
+
 class FalconLLM(LLM):
     """
     A class representing a custom Falcon language model.
 
     Attributes:
-        n (int): A parameter for the LLM operation.
+        falcon_model_dir (str): The directory of the Falcon model.
+        falcon_tokenizer (AutoTokenizer): The tokenizer for the Falcon model.
+        falcon_pipeline (Any): The pipeline for the Falcon model.
+        db (db): The database for the Falcon model.
+        endpoint (Any): The endpoint for the Falcon model.
+        voice (Speaker): The speaker for the Falcon model.
     """
+
+    # Define the default directory for the Falcon model
     falcon_model_dir: str = Field(default="tiiuae/falcon-7b-instruct")
-    falcon_tokenizer: AutoTokenizer = None 
-    falcon_pipeline: Any = None  # TODO: find the proper type for this
+    falcon_tokenizer: AutoTokenizer = None
+    falcon_pipeline: Any = None
     db: db = None
     endpoint: Any = None
     voice: Speaker = None
 
     def __init__(self):
+        """
+        Initializes the FalconLLM class.
+        """
         super().__init__()  # Call LLM's __init__ method
-        self.falcon_tokenizer = AutoTokenizer.from_pretrained(self.falcon_model_dir)
+        # Load the tokenizer from the Falcon model directory
+        self.falcon_tokenizer = AutoTokenizer.from_pretrained(
+            self.falcon_model_dir
+        )
+        # Create a pipeline for text generation using the Falcon model
         self.falcon_pipeline = pipeline(
             "text-generation",
             model=self.falcon_model_dir,
             tokenizer=self.falcon_tokenizer,
             torch_dtype=bfloat16,
             trust_remote_code=True,
-            device_map="auto"
+            device_map="auto",
         )
+        # Initialize the database
         self.db = db()
-        self.endpoint = RetrievalQA.from_chain_type(llm=self, chain_type="stuff", retriever=self.db.retriever())
-        self.voice = Speaker("/home/ubuntu/Tehas/lippy/data/audio")
+        # Create an endpoint for the Falcon model
+        self.endpoint = RetrievalQA.from_chain_type(
+            llm=self, chain_type="stuff", retriever=self.db.retriever()
+        )
+        # Initialize the speaker and load the model
+        self.voice = Speaker(str(PROJ_DIR / "data/audio"))
         self.voice.load_model()
 
     @property
@@ -50,17 +73,18 @@ class FalconLLM(LLM):
         return "custom"
 
     def _call(
-            self,
-            prompt: str,
-            speak: bool = True,
-            stop: List[str] = None,
-            run_manager: CallbackManagerForLLMRun = None,
+        self,
+        prompt: str,
+        speak: bool = True,
+        stop: List[str] = None,
+        run_manager: CallbackManagerForLLMRun = None,
     ) -> str:
         """
         Call the LLM with provided parameters.
 
         Args:
             prompt (str): The input for the LLM.
+            speak (bool): Whether to speak the output. Defaults to True.
             stop (List[str]): Not permitted, raises ValueError if provided.
             run_manager (CallbackManagerForLLMRun, optional): A callback
                 manager.
@@ -71,35 +95,36 @@ class FalconLLM(LLM):
         Raises:
             ValueError: If the stop argument is provided.
         """
+        # Raise an error if the stop argument is provided
         if stop is not None:
             raise ValueError("stop kwargs are not permitted.")
-        
+
+        # Generate sequences using the Falcon pipeline
         sequences = self.falcon_pipeline(
             prompt,
             max_length=8000,
             do_sample=True,
             top_k=10,
             num_return_sequences=1,
-            eos_token_id=self.falcon_tokenizer.eos_token_id
+            eos_token_id=self.falcon_tokenizer.eos_token_id,
         )
-        resp = ", ".join([seq['generated_text'] for seq in sequences])
-        answer = resp.split('Helpful Answer:')[1]
+        # Join the generated sequences into a response
+        resp = ", ".join([seq["generated_text"] for seq in sequences])
+        # Extract the answer from the response
+        answer = resp.split("Helpful Answer:")[1]
         print(answer)
+        # Use the speaker to say the answer
         self.voice.say(answer)
         return answer
 
-    
-    # @property
-    # def _identifying_params(self) -> Mapping[str, Any]:
-    #     """
-    #     Get the identifying parameters of the LLM.
 
-    #     Returns:
-    #         Mapping[str, Any]: Dictionary with identifying parameters.
-    #     """
-    #     return {"n": self.n}
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Initialize the FalconLLM
     llm = FalconLLM()
-    print(llm.endpoint.run(("What impacts the pH of a sample? Please provide a detailed answer.")))
-     
+    # Run the endpoint with a prompt and print the result
+    print(
+        llm.endpoint.run(
+            ("What impacts the pH of a sample? Please provide a detailed "
+             "answer.")
+        )
+    )
