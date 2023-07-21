098fab6eaeaf5c8624d188bb4b0ddaa186495a17
JordieB <jordie.r.belle@gmail.com>
2023-07-20 21:53:30 -0700

    refactored vector_store py

--- lippy/utils/vector_store.py
+++ lippy/utils/vector_store.py
@@ -1,54 +1,66 @@
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceInstructEmbeddings
-# from langchain.text_splitter import MarkdownHeaderTextSplitter
-from langchain.document_loaders import UnstructuredMarkdownLoader, DirectoryLoader
-# import langchain
-# import inspect
-# print(inspect.getfile(MarkdownHeaderTextSplitter))
 from lippy.utils.obsidian_loader import ObsidianLoader
+from pathlib import Path
 
-# Injest documents
-# Provide retriever for use in RetrievalQA chain
-headers_to_split_on = [
-    ("#", "Header 1"),
-    ("##", "Header 2"),
-    ("###", "Header 3"),
-    ("####", "Header 4"),
-    ("#####", "Header 5"),
-]
+PROJ_DIR = Path(__file__).resolve().parents[2]
 
-class db:
-    def __init__(self,chunkSize=500):
-        self.pathDB = "/home/ubuntu/Tehas/lippy/data/db"
+class Database:
+    """
+    A class to represent a database for document retrieval.
+
+    Attributes:
+        pathDB (str): Path to the database.
+        embeddings (HuggingFaceInstructEmbeddings): Embeddings to use for the documents.
+        vectordb (Chroma): Vector database for the documents.
+    """
+
+    def __init__(self, chunk_size=500):
+        """
+        Initializes the Database object.
+
+        Args:
+            chunk_size (int, optional): Chunk size for the documents. Defaults to 500.
+        """
+        self.pathDB = str(PROJ_DIR / "data/db")
         self.embeddings = HuggingFaceInstructEmbeddings()
         self.vectordb = None
 
-    def injest(self, path="/home/ubuntu/Tehas/lippy/data/vault/2 - Notes"):
-        self.pathData = path
+    def injest(self, path=str(PROJ_DIR / "data/vault/2 - Notes")):
+        """
+        Ingests the documents from the specified path into the vector database.
+
+        Args:
+            path (str, optional): Path to the documents. Defaults to 'data/vault/2 - Notes'.
+        """
         self.loader = ObsidianLoader(path)
         docs = self.loader.load()
-        self.vectordb = Chroma.from_documents(documents=docs, embedding=self.embeddings, persist_directory=self.pathDB)
+        self.vectordb = Chroma.from_documents(
+            documents=docs, embedding=self.embeddings, persist_directory=self.pathDB
+        )
 
     def retriever(self):
-        self.injest()
-        return self.vectordb.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .5, "k":5}) if self.vectordb is not None else None
+        """
+        Returns a retriever for the vector database.
 
-if __name__ == '__main__':
-    # from falcon_llm import FalconLLM
-    # llm = FalconLLM()
-    # loader = ObsidianLoader("/home/theatasigma/lippy/data/vault/2 - Notes")
-    # docs = loader.load()
-    db = db()
-    db.pathDB="/home/theatasigma/lippy/data/db" 
-    db.injest("/home/theatasigma/lippy/data/vault/2 - Notes")
-    # ret = db.vectordb.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .5, "k":5})
-    # docs = ret.get_relevant_documents("what are the effects of DO on water?")
-    docs = db.vectordb.similarity_search_with_score("what are the effects of DO on water?", k=5)
-    
-    # print(inspect.getsource(ObsidianLoader))
-    
-    # texts = db.splitter.split_texts(docs)
-    print(db.vectordb._collection_)
-    # for i in range(len(docs)):
-    #     print("-----")
-    #     print(docs[i])
+        Returns:
+            Retriever: The retriever for the vector database, or None if the database is not initialized.
+        """
+        self.injest()
+        return (
+            self.vectordb.as_retriever(
+                search_type="similarity_score_threshold",
+                search_kwargs={"score_threshold": 0.5, "k": 5},
+            )
+            if self.vectordb is not None
+            else None
+        )
+
+
+if __name__ == "__main__":
+    db = Database()
+    db.injest()
+    docs = db.vectordb.similarity_search_with_score(
+        "what are the effects of DO on water?", k=5
+    )
+    print(db.vectordb._collection)
